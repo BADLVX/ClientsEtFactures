a0057e8b18780879074d79dd70eae804bd54b1ae
BADLVX <HugoYoucefi0@gmail.com>
2023-12-04 12:08:18 +0100

    Create MonException.java
    Creation de l'exception pour MontantFactureNegatif

---
+++ src/metier/MonException.java
@@ -0,0 +1,15 @@
+package metier;
+
+public class MonException extends Exception {
+	
+	public MonException()
+    {
+        System.out.println("Exception monException has been raised...");
+    }
+    
+    public String toString()
+    {
+      return "You tried to do an illegal assignement !";
+    }
+
+}
